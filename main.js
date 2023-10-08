fae3372bcc896a5a2bcdbcb32350684ce8ba6f7c
Marti Paredes <martiparedes19@gmail.com>
2023-10-08 22:00:22 +0200

    Remake main.js

--- main.js
+++ main.js
@@ -1,13 +1,163 @@
 import { appendSeasonsDropdown } from "./js/seasonsList.js";
 import { getSeasonsList } from "./js/seasonsList.js";
-import { mounthighcharts } from "./js/mountHighcharts.js";
+import { getDriversNationailty } from "/js/driversNationality.js";
+import { getDriversWins } from "/js/driversWin.js";
+import { getDriversEvolution } from "/js/driversEvolution.js";
 
 const pageLoader = document.getElementById('full-page-loader')
 pageLoader.style.opacity = '100'
 pageLoader.style.display = 'grid'
 
-let seasonsList = null
+async function mountHighcharts(season) {
+    const [drivers, wins, evolution] = await Promise.all([getDriversNationailty(season), getDriversWins(season), getDriversEvolution(season)]);
 
+
+    document.getElementById('container-pie-drivers').innerHTML=""
+    document.getElementById('container-bars-wins').innerHTML=""
+    document.getElementById('container-chart-evolution').innerHTML=""
+
+    Highcharts.chart('container-pie-drivers', {
+        chart: {
+            type: 'variablepie'
+        },
+        title: {
+            text: '',
+            align: 'left'
+        },
+        tooltip: {
+            headerFormat: '',
+            formatter: function () {
+                var punto = this.point;
+        
+                // Obtenemos la lista de nombres de pilotos para esta nacionalidad
+                var pilotos = punto.drivers.join(', ');
+        
+                var tooltipContent = '<span style="color:' + punto.color + '">\u25CF</span> <b>' + punto.name + '</b><br/>' +
+                    'Número: <b>' + punto.z + '</b><br/>' +
+                    // Agregamos los nombres de pilotos relacionados con esta nacionalidad
+                    'Pilotos: <b>' + pilotos + '</b><br/>';
+                
+                return tooltipContent;
+            }
+        },
+        
+        series: [{
+            minPointSize: 10,
+            innerSize: '20%',
+            zMin: 0,
+            data: drivers,
+        }]
+    });
+
+    Highcharts.chart('container-bars-wins', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: ''
+        },
+        xAxis: {
+            type: 'category',
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: ''
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            headerFormat: '',
+            pointFormat: 'Victorias: <b>{point.y}</b>'
+        },
+        series: [{
+            name: 'Victorias',
+            colorByPoint: true,
+            data: wins,
+            dataLabels: {
+                enabled: true,
+                align: 'center',
+                format: '{point.y}',
+            }
+        }]
+    });
+
+
+    Highcharts.chart('container-chart-evolution', {
+        chart: {
+            type: 'spline'
+        },
+        title: {
+            text: '',
+        },
+        yAxis: {
+            title: {
+                text: ''
+            },
+        },
+        xAxis: {
+            categories: evolution.dates.map(item => item.date), 
+        },
+        plotOptions: {
+            series: {
+                label: {
+                    connectorAllowed: false
+                }
+            },
+            spline: {
+                lineWidth: 2,
+                marker: {
+                    enabled: false
+                },
+            }
+        },
+        tooltip: {
+            formatter: function () {
+                var date = this.key;
+                var seriesName = this.series.name;
+                var carrera = '';
+
+                var dateIndex = evolution.dates.findIndex(item => item.date === date);
+                if (dateIndex !== -1) {
+                    carrera = evolution.dates[dateIndex].race;
+                }
+
+                var tooltipContent = '<span style="color:' + this.color + '">\u25CF</span> <b>' + seriesName + '</b><br/>Puntos acumulados: ' + this.y + '<br/>Carrera: ' + carrera;
+
+                return tooltipContent;
+            }
+        },
+        legend: {
+            enabled: true
+        },
+        series: evolution.drivers,
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        enabled: false,
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
+                    }
+                }
+            }]
+        }
+    });
+
+
+    Array.from(document.querySelectorAll('.highcharts-credits')).map((item) => {
+        item.style.display = 'none';
+    });
+
+};
+
+let seasonsList = null
 document.addEventListener('DOMContentLoaded', async function () {   
     if(!seasonsList) {
         seasonsList = await getSeasonsList()
@@ -15,11 +165,11 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     document.getElementById('seasons').addEventListener('change', async (event) => {
         const pageLoader = document.getElementById('full-page-loader')
-        pageLoader.style.display = 'grid'
         pageLoader.style.opacity = '100'
+        pageLoader.style.display = 'grid'
 
         
-        await mounthighcharts(event.target.value)
+        await mountHighcharts(event.target.value)
 
         pageLoader.style.opacity = '0'
         await setTimeout(function () {
@@ -28,7 +178,7 @@ document.addEventListener('DOMContentLoaded', async function () {
     })
 
     await appendSeasonsDropdown(seasonsList)
-    await mounthighcharts('current')
+    await mountHighcharts('current')
 
     pageLoader.style.opacity = '0'
     await setTimeout(function () {
